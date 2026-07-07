68abea87960ff76395743bcdafdd237e9bceec09
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-07 17:00:41 -0400

    Uso de statements

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const x = 4;
+  let y = 'Apple';
+
+  if (x < 10) {
+    y = 'Banana';
+  }
+
+  const happy = true;
+
   return (
     <section className="container my-2">
-      <p>Layout</p>
+      <p>Fruit: {y}</p>
+
+      <p>Fruit: {(x < 5) ? 'Pear' : 'Orange'}</p>
+
+      <hr/>
+
+      <p>{happy ? 1 : 0}</p>
     </section>
   )
 }
